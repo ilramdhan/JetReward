11610d1926d9f52eec2f7604e24ac452f17d532f
Ilham Ramadhan <ilhamram332@gmail.com>
2023-11-29 16:05:43 +0700

    Initial commit

---
+++ app/src/main/java/com/dicoding/jetreward/JetRewardApp.kt
@@ -0,0 +1,82 @@
+@file:Suppress("UNUSED_EXPRESSION")
+
+package com.dicoding.jetreward
+
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.AccountCircle
+import androidx.compose.material.icons.filled.Home
+import androidx.compose.material.icons.filled.ShoppingCart
+import androidx.compose.material3.Icon
+import androidx.compose.material3.NavigationBar
+import androidx.compose.material3.NavigationBarItem
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.tooling.preview.Preview
+import com.dicoding.jetreward.ui.navigation.NavigationItem
+import com.dicoding.jetreward.ui.theme.JetRewardTheme
+import com.dicoding.jetreward.ui.navigation.Screen
+
+@Composable
+private fun BottomBar(
+    modifier: Modifier = Modifier
+) {
+    NavigationBar(
+        modifier = modifier,
+    ) {
+        val navigationItems = listOf(
+            NavigationItem(
+                title = stringResource(R.string.menu_home),
+                icon = Icons.Default.Home,
+                screen = Screen.Home
+            ),
+            NavigationItem(
+                title = stringResource(R.string.menu_cart),
+                icon = Icons.Default.ShoppingCart,
+                screen = Screen.Cart
+            ),
+            NavigationItem(
+                title = stringResource(R.string.menu_profile),
+                icon = Icons.Default.AccountCircle,
+                screen = Screen.Profile
+            ),
+        )
+        navigationItems.map { item ->
+            NavigationBarItem(
+                icon = {
+                    Icon(
+                        imageVector = item.icon,
+                        contentDescription = item.title
+                    )
+                },
+                label = { Text(item.title) },
+                selected = false,
+                onClick = {
+                }
+            )
+        }
+    }
+}
+
+@Composable
+fun JetRewardApp(
+    modifier: Modifier = Modifier,
+) {
+    Scaffold(
+        bottomBar = {
+            BottomBar()
+        },
+        modifier = modifier
+    ) {it
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun JetHeroesAppPreview() {
+    JetRewardTheme {
+        JetRewardApp()
+    }
+}
